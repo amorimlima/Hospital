429c854d8dd834cdf7e498f32dc73e5abce96963
Nilton Leme Pereira <amorim@muranodesign.com.br>
2015-07-08 13:05:17 -0300

    Delete funcoes.js

--- js/funcoes.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function(){
-   $("#eventos").corner("8px");	
-    $("#boxTextoCidade").corner("8px");	
-	 $("#boxPrevGaleria").corner("8px");	
-	  $("#boxPrevDestaque").corner("8px");	
-});
