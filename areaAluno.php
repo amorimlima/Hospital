8a91efd16cb99c5e0a6cff5a036477f30fce1dce
Lucas Tavares <lucas@muranodesign.com.br>
2015-09-22 13:25:33 -0300

    títulos áreas

--- areaAluno.php
+++ areaAluno.php
@@ -21,7 +21,7 @@ $templateGeral = new Template();
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
-    <title>Home</title>
+    <title>Area Aluno</title>
 
     <!-- Bootstrap -->
     <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
--- areaProfessor.php
+++ areaProfessor.php
@@ -18,7 +18,7 @@ $templateGeral = new Template();
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
-        <title>Área Hospital</title>
+        <title>Área Professor</title>
         <!-- Bootstrap -->
         <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
         <link rel="stylesheet" type="text/css" href="css/style.css">
