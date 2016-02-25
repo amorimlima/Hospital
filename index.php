b0e5d08b99243b997938270ae4d5b5e67c35d885
Lucas Tavares <amorim@muranodesign.com.br>
2016-02-25 11:42:03 -0300

    Tela de login

--- index.php
+++ index.php
@@ -6,7 +6,9 @@ require_once '_loadPaths.inc.php';
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
 <title>Login</title>
-<link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
+<link href="bootstrap/css/bootstrap.min.css" rel="stylesheet" />
+<link href="css/style.css" rel="stylesheet" />
+<link href="css/index.css" rel="stylesheet" />
 
 <script type="text/javascript" src="js/jquery-1.10.2.js"></script>
 <!-- -->
@@ -42,35 +44,46 @@ $(document).ready(function(){
 </script>
 </head>
 <body>
-    <!--login modal-->
-    <div id="loginModal" class="modal show" tabindex="-1" role="dialog" aria-hidden="true">
-      <div class="modal-dialog">
-      <div class="modal-content">
-          <div class="modal-header">
-              <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
-              <h1 class="text-center">Login</h1>
-          </div>
-          <div class="modal-body">
-              <form class="form center-block">
-                <div class="form-group">
-                  <input type="text" class="form-control input-lg form-actions" name="usuario" id="usuario" value="hcbAluno">
-                </div>
-                <div class="form-group">
-                    <input type="password" class="form-control input-lg form-actions" name="senha" id="senha" value="123">
-                </div>
-                <div class="form-group">
-                  <button class="btn btn-primary btn-lg btn-block"  id="btLogar">Logar</button>
-                </div>
-              </form>
-          </div>
-          <div class="modal-footer">
-              <div class="col-md-12">
-              <button class="btn" data-dismiss="modal" aria-hidden="true">Cancel</button>
-              </div>	
-          </div>
-      </div>
-      </div>
-    </div>
+<div class="container">
+	<div class="row">
+		<div class="col-xs-12">
+			<div class="logo-container">
+				<img src="img/logo/logo_lg.png">
+			</div>
+		</div>
+	</div>
+	<div class="row">
+		<div class="col-md-2">&nbsp;</div>
+		<div class="col-md-4">
+			<div class="login-panel-container">
+				<div class="panel panel-default">
+					<div class="panel-body">
+						<form class="form center-block">
+							<div class="form-group">
+								<label for"usuario">USUÁRIO</label>
+								<input type="text" class="form-control input-lg form-actions" name="usuario" id="usuario" value="hcbAluno">
+							</div>
+							<div class="form-group">
+								<label for"usuario">SENHA</label>
+								<input type="password" class="form-control input-lg form-actions" name="senha" id="senha" value="123">
+							</div>
+							<div class="form-group">
+								<button class="btn btn-primary btn-lg" id="btLogar">Entrar</button>
+							</div>
+						  </form>
+						  <div class="link">
+						  	<a href="#">Esqueceu a senha?</a>
+						  </div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="col-md-4">
+			<div class="pratinha"></div>
+		</div>
+		<div class="col-md-2">&nbsp;</div>
+	</div>
+</div>
 </body>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
 <script src="//code.jquery.com/ui/1.11.4/jquery-ui.js"></script>
