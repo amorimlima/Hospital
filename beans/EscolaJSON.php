4e380929fc43941e66bc43b1963281776c2b2dd7
Diego Garcia <diegomg0301@gmail.com>
2016-06-06 11:56:23 -0300

    EscolaJSON

--- beans/EscolaJSON.php
+++ beans/EscolaJSON.php
@@ -15,31 +15,31 @@ class EscolaJSON {
 	private $ejs_escola;
 	private $ejs_string;
 
-	public function EscolaJSON {}
+	public function EscolaJSON() {}
 
 	public function getEjs_id() {
-		return $this->$ejs_id;
+		return $this->ejs_id;
 	}
 
 	public function getEjs_escola() {
-		return $this->$ejs_escola;
+		return $this->ejs_escola;
 	}
 
 	public function getEjs_string() {
-		return $this->$ejs_string;
+		return $this->ejs_string;
 	}
 
 
 	public function setEjs_id($id) {
-		$this->$ejs_id = $id;
+		$this->ejs_id = $id;
 	}
 
 	public function setEjs_escola($escola) {
-		$this->$ejs_escola = $escola;
+		$this->ejs_escola = $escola;
 	}
 
 	public function setEjs_string($string) {
-		$this->$ejs_string = $string;
+		$this->ejs_string = $string;
 	}
 }
 
